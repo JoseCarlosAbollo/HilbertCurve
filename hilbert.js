e7702007a4c22f7dec51c43e58bb09b5120a8451
José Carlos <79134998+JoseCarlosAbollo@users.noreply.github.com>
2021-03-12 13:31:22 +0100

    Update hilbert.js

--- hilbert.js
+++ hilbert.js
@@ -1,16 +1,16 @@
-var pic;						// Image container
-var imgSize = 900;				// Size of the image
-var order = 7;					// Number of steps to shape the curve
+var pic;			// Image container
+var imgSize = 900;		// Size of the image
+var order = 7;			// Number of steps to shape the curve
 var sectr = Math.pow(2, order);	// Number of sectors divided in the plane (4 per step)
-var len = imgSize/sectr;		// Size of a sector
-var N = sectr * sectr;			// Number of vertices
-var points = [];				// Map of vertices
-var animation;					// 
-var current = 1;				// The current vertex at any time in execution
+var len = imgSize/sectr;	// Size of a sector
+var N = sectr * sectr;		// Number of vertices
+var points = [];		// Map of vertices
+var animation;			// Animation flag
+var current = 1;		// The current vertex at any time in execution
 
 // Preload desired picture
 function preload() {
-  //pic = loadImage("picture.jpg"); //ERROR
+  pic = loadImage("picture.jpg"); //ERROR
 }
 
 function setup(){
@@ -18,7 +18,7 @@ function setup(){
 	let img = createImage(imgSize, imgSize);
 	createCanvas(img.width, img.height);
 	// Place the picture and resize it to fit measurements
-	//image(pic, 0, 0, imgSize, imgSize); //ERROR
+	image(pic, 0, 0, imgSize, imgSize); //ERROR
 	// Add a black background on top to block picture
 	background(0);
 	// Set the lines width and color
@@ -34,11 +34,11 @@ function setup(){
 }
 
 function draw(){
-	// First for loop shows all curve at once, second loop draw it in animation
+	// First for loop shows all curve at once, second loop draws it in an animation
 	//for(let i = 0; i < N-1; ++i){ animation = false;
 	for(let i = 0; i < current; ++i){ animation = true;
 		// Pick the color from the picture's pixel placed in the vertex
-		//let color = img.get(int(points[i][0]),int(points[i][1])); //ERROR
+		let color = img.get(int(points[i][0]),int(points[i][1])); //ERROR
 		line(points[i][0], points[i][1], points[i+1][0], points[i+1][1]);
 	}
 	// Update the counter for animation
